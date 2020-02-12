460e7747fa5188c2d7a432e01aa2b96698b33d3c
Fernando Urbina <ferurbi2000@hotmail.com>
2020-02-11 18:23:41 -0600

    Nuevos Cambios

--- AprendiendoMVC5/HolaMundo/Controllers/HomeController.cs
+++ AprendiendoMVC5/HolaMundo/Controllers/HomeController.cs
@@ -42,8 +42,9 @@ namespace HolaMundo.Controllers
             var persona2 = new Persona { Name = "Heydi Lanzas", Age = 39 };
             var persona3 = new Persona { Name = "Fernando Urbina", Age = 42 };
             var persona4 = new Persona { Name = "Heydi Lanzas", Age = 39 };
+            var persona5 = new Persona { Name = "Maria Fernando", Age = 17 };
 
-            return Json(new List<Persona> { persona1, persona2, persona3, persona4 }, JsonRequestBehavior.AllowGet);
+            return Json(new List<Persona> { persona1, persona2, persona3, persona4, persona5}, JsonRequestBehavior.AllowGet);
         }
 
         public ContentResult Test()
